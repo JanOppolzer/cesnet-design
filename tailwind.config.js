67efdefea898c67827c1db33b4d80d9e7da1e277
Jan Oppolzer <jan@oppolzer.cz>
2021-03-03 15:02:31 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  purge: [
+    './*.html',
+  ],
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'xm': '860px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
+    extend: {
+      colors: {
+        warmGray: colors.warmGray,
+        cesnetDark: '#4b4d4a',
+        cesnetGray: '#dcdcda',
+        cesnetRed: '#e60000',
+        cesnetLightRed: '#f6d1c3',
+      },
+      listStyleType: {
+        square: 'square'
+      },
+      fontFamily: {
+        'cesnet': ['Oswald', 'Arial', 'sans-serif'],
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
